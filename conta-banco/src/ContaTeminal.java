5ad0c8238bd6e58819a895a972a5b689563311e9
V1nic1us <marcusvinicius.mds16@gmail.com>
2024-07-09 12:08:26 -0400

    feat: Update ContaTeminal.java to prompt user for account details

--- conta-banco/src/ContaTeminal.java
+++ conta-banco/src/ContaTeminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTeminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta:");
+        int numeroDaConta = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite o número da Agência:");
+        String numeroDaAgencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente:");
+        String nomeDoCliente = scanner.nextLine();
+
+        System.out.println("Digite o numero da conta:");
+        float saldo = Float.parseFloat(scanner.nextLine());
+
+        scanner.close();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2123f já está disponível para saque", nomeDoCliente, numeroDaAgencia, numeroDaConta, saldo);
     }
 }
